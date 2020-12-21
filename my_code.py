ea1d4be76fbab6aeeee936741218f3f40651caaf
nnigmat <nnigmat@gmail.com>
2020-12-21 21:06:03 +0000

    .|

--- my_code.py
+++ my_code.py
@@ -4,5 +4,5 @@ from os import system
 epochs = 100
 t = Template('python3 src/main.py --content_path images/input/frame${index}.jpg --style_path images/fire/frame${index}.jpg --max_epochs ${epochs} --output_path images/outputs/output${index}.jpg')
 
-for i in range(1):
+for i in range(1130):
     system(t.substitute(index=i, epochs=epochs))
