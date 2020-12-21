bbbe3c2f66ff4c7735542cd73e49fd58d67254d2
nnigmat <nnigmat@gmail.com>
2020-12-21 20:22:58 +0000

    .

---
+++ my_code.py
@@ -0,0 +1,8 @@
+from string import Template
+from os import system
+
+epochs = 100
+t = Template('python3 src/main.py --content_path images/input/frame${index}.jpg --style_path images/fire/frame${index}.jpg --max_epochs ${epochs} --output_path images/outputs/output${index}.jpg')
+
+for i in range(1):
+    system(t.substitute(index=i, epochs=epochs))
