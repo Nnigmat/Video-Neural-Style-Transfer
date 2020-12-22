71ed8eee57bb727caab3ab398d840eabb293a240
nnigmat <nnigmat@gmail.com>
2020-12-22 06:37:07 +0000

    .

--- my_code.py
+++ my_code.py
@@ -1,8 +1,14 @@
 from string import Template
 from os import system
+from multiprocessing import Pool
 
 epochs = 80
 t = Template('python3 src/main.py --content_path images/input/frame${index}.jpg --style_path images/fire/frame${index}.jpg --max_epochs ${epochs} --output_path images/outputs/output${index}.jpg')
 
-for i in range(33, 1130):
-    system(t.substitute(index=i, epochs=epochs))
+def process(index):
+    system(t.substitute(index=index, epochs=epochs))
+
+indicies = [i for i in range(13, 1130)]
+with Pool(10) as p:
+    p.map(process, indicies)
+    
